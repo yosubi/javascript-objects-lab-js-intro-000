b7c0607598ed601e1faa26c7740ea18dae8358b6
Joseph Moon <nakkta@gmail.com>
2019-09-27 20:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign ({}, {key: [value]})
-  updateObjectWithKeyAndValue(obj, key, value)
-  obj
+  return Object.assign ({}, {]key]: value})
+
 }
