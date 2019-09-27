e7abb3deb6c6f3f0bd10866350ab9a256f151f64
Joseph Moon <nakkta@gmail.com>
2019-09-27 21:04:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
 
